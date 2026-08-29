dd88781df60260f47358bd01d6dbd441c410fa5b
Sajjad Ahmed Soomro <SSOOMRO@beckman.com>
2026-08-29 02:55:13 +0200

    build: upgrade application to .NET 10

--- HospitalManagement/HospitalManagement.App/Styles/CustomLegends.cs
+++ HospitalManagement/HospitalManagement.App/Styles/CustomLegends.cs
@@ -1,107 +1,11 @@
-﻿using LiveChartsCore.Drawing;
-using LiveChartsCore.Kernel.Events;
-using LiveChartsCore.Kernel.Sketches;
-using LiveChartsCore.Measure;
-using LiveChartsCore.SkiaSharpView.Drawing.Geometries;
-using LiveChartsCore.SkiaSharpView.Drawing;
-using LiveChartsCore.SkiaSharpView.Painting;
-using LiveChartsCore.SkiaSharpView.VisualElements;
-using LiveChartsCore.VisualElements;
-using LiveChartsCore;
-using SkiaSharp;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.SKCharts;
 
 namespace HospitalManagement.App.ViewModels
 {
-    public class CustomLegend : IChartLegend<SkiaSharpDrawingContext>
+    /// <summary>
+    /// Provides a XAML-addressable legend using LiveCharts' supported legend implementation.
+    /// </summary>
+    public class CustomLegend : SKDefaultLegend
     {
-        private static readonly int s_zIndex = 10050;
-        private readonly StackPanel<RoundedRectangleGeometry, SkiaSharpDrawingContext> _stackPanel = new();
-        private readonly SolidColorPaint _fontPaint = new(new SKColor(30, 20, 30))
-        {
-            SKTypeface = SKTypeface.FromFamilyName("Arial", SKFontStyle.Bold),
-            ZIndex = s_zIndex + 1
-        };
-
-        public void Draw(Chart<SkiaSharpDrawingContext> chart)
-        {
-            var legendPosition = chart.GetLegendPosition();
-
-            _stackPanel.X = legendPosition.X;
-            _stackPanel.Y = legendPosition.Y;
-
-            chart.AddVisual(_stackPanel);
-            if (chart.LegendPosition == LegendPosition.Hidden) chart.RemoveVisual(_stackPanel);
-        }
-
-        public LvcSize Measure(Chart<SkiaSharpDrawingContext> chart)
-        {
-            _stackPanel.Orientation = ContainerOrientation.Vertical;
-            _stackPanel.MaxWidth = double.MaxValue;
-            _stackPanel.MaxHeight = chart.ControlSize.Height;
-
-            // clear the previous elements.
-            foreach (var visual in _stackPanel.Children.ToArray())
-            {
-                _ = _stackPanel.Children.Remove(visual);
-                chart.RemoveVisual(visual);
-            }
-
-            var theme = LiveCharts.DefaultSettings.GetTheme<SkiaSharpDrawingContext>();
-
-            foreach (var series in chart.Series.Where(x => x.IsVisibleAtLegend))
-            {
-                var panel = new StackPanel<RectangleGeometry, SkiaSharpDrawingContext>
-                {
-                    Padding = new Padding(12, 6),
-                    VerticalAlignment = Align.Middle,
-                    HorizontalAlignment = Align.Middle,
-                    Children =
-                {
-                    new SVGVisual
-                    {
-                        Path = SKPath.ParseSvgPathData(SVGPoints.Circle),
-                        Width = 10,
-                        Height = 10,
-                        ClippingMode = ClipMode.None, // required on legends // mark
-                        Fill = new SolidColorPaint(theme.GetSeriesColor(series).AsSKColor())
-                        {
-                            ZIndex = s_zIndex + 1
-                        }
-                    },
-                    // series.GetMiniature(null, s_zIndex), or get the miniature defined in the series.
-                    new LabelVisual
-                    {
-                        Text = series.Name ?? string.Empty,
-                        Paint = _fontPaint,
-                        TextSize = 10,
-                        ClippingMode = ClipMode.None, // required on legends // mark
-                        Padding = new Padding(8, 0, 0, 0),
-                        VerticalAlignment = Align.Start,
-                        HorizontalAlignment = Align.Start
-                    }
-                }
-                };
-
-                panel.PointerDown += GetPointerDownHandler(series);
-                _stackPanel.Children.Add(panel);
-            }
-
-            return _stackPanel.Measure(chart);
-        }
-
-        private static VisualElementHandler<SkiaSharpDrawingContext> GetPointerDownHandler(
-            IChartSeries<SkiaSharpDrawingContext> series)
-        {
-            return (visual, args) =>
-            {
-                series.IsVisible = !series.IsVisible;
-            };
-        }
     }
 }
